ca37deb76879eafe22f02d7228bf554182937609
Jesper Paardekooper <j.paardekooper@grandslam-media.nl>
2017-02-07 14:53:21 +0100

    Fix responsive image view helper

--- Classes/ViewHelper/ResponsiveBackgroundImageViewHelper.php
+++ Classes/ViewHelper/ResponsiveBackgroundImageViewHelper.php
@@ -6,16 +6,17 @@ namespace DevElement\DevelementResponsiveBackgroundImage\ViewHelper;
  *
  * @package DevElement\DevelementResponsiveBackgroundImage\ViewHelper
  */
-class ResponsiveBackgroundImageViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper
+class ResponsiveBackgroundImageViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper
 {
     /**
      * @var array
      */
     protected $breakPointConfig = [
-        1199       => ['min-width' => '992px', 'max-width' => '1199px'],
-        991        => ['min-width' => '768px', 'max-width' => '991px'],
-        767        => ['min-width' => '480px', 'max-width' => '767px'],
-        479        => ['max-width' => '479px'],
+        'lg'        => null,
+        'md'        => 1199,
+        'sm'        => 991,
+        'xs'        => 767,
+        'low-res'   => 50
     ];
 
     /**
@@ -25,64 +26,58 @@ class ResponsiveBackgroundImageViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelp
     protected $imageService;
 
     /**
+     * @var string
+     */
+    protected $tagName = 'div';
+
+    /**
+     * Initialize arguments.
+     *
+     * @return void
+     */
+    public function initializeArguments()
+    {
+        parent::initializeArguments();
+        $this->registerUniversalTagAttributes();
+    }
+
+    /**
      * @param \TYPO3\CMS\Core\Resource\FileInterface $image
-     * @param string $id
      * @param array $includedBreakPoints
      * @return string
      */
-    public function render($image, $id, $includedBreakPoints = [1199, 991, 767, 479])
+    public function render($image, $includedBreakPoints = ['lg', 'md', 'sm', 'xs', 'low-res'])
     {
         if ($image instanceof \TYPO3\CMS\Core\Resource\FileReference) {
-            $css = '<style type="text/css">';
-
-            // Render the default
-            $css .= $this->renderCssTag($image, $id);
-
             foreach ($includedBreakPoints as $breakPoint) {
-                if (isset($this->breakPointConfig[$breakPoint])) {
-                    $processingInstructions = [
-                        'width' => (int)$breakPoint
-                    ];
+                if (array_key_exists($breakPoint, $this->breakPointConfig)) {
+                    if ($breakPoint === 'lg') {
+                        // Original is lg
+                        $this->tag->addAttribute('data-lg', '/' . $image->getPublicUrl());
+                    } else {
+                        $processingInstructions = [
+                            'width' => (int)$this->breakPointConfig[$breakPoint]
+                        ];
 
-                    $processedImage = $this->imageService->applyProcessingInstructions($image, $processingInstructions);
-                    if ($processedImage instanceof \TYPO3\CMS\Core\Resource\ProcessedFile) {
-                        $mediaQuery = '';
-                        if (isset($this->breakPointConfig[$breakPoint]['min-width']) && isset($this->breakPointConfig[$breakPoint]['max-width'])) {
-                            $mediaQuery = '@media (min-width: ' . $this->breakPointConfig[$breakPoint]['min-width'] . ') and (max-width: ' . $this->breakPointConfig[$breakPoint]['max-width'] . ') {';
-                        } elseif (isset($this->breakPointConfig[$breakPoint]['max-width'])) {
-                            $mediaQuery = '@media (max-width: ' . $this->breakPointConfig[$breakPoint]['max-width']. ') {';
+                        $processedImage = $this->imageService->applyProcessingInstructions($image, $processingInstructions);
+                        if ($processedImage instanceof \TYPO3\CMS\Core\Resource\ProcessedFile) {
+                            if ($breakPoint === 'low-res') {
+                                // Low res will be rendered inline as base64
+                                $base64 = 'data:' . $image->getMimeType() . ';base64,' . base64_encode($processedImage->getContents());
+                                $this->tag->addAttribute('style', 'background-image: url(' . $base64 . ')');
+                            } else {
+                                // All others will be data attributes
+                                $this->tag->addAttribute('data-' . $breakPoint, '/' . $processedImage->getPublicUrl());
+                            }
                         }
-
-                        $css .= $mediaQuery;
-                        $css .= $this->renderCssTag($processedImage, $id);
-                        $css .= '}';
                     }
                 }
             }
-            $css .= '</style>';
-            $this->getTypoScriptFrontendController()->additionalHeaderData[] .= $css;
         }
-        return $this->renderChildren();
-    }
 
-    /**
-     * @param \TYPO3\CMS\Core\Resource\FileInterface $image
-     * @param int $id
-     * @return string
-     */
-    protected function renderCssTag(\TYPO3\CMS\Core\Resource\FileInterface $image, $id)
-    {
-        $css = '#' . $id . '{';
-        $css .= 'background-image: url(/' . $image->getPublicUrl() . ');';
-        $css .= '}';
-        return $css;
-    }
-
-    /**
-     * @return \TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController
-     */
-    protected function getTypoScriptFrontendController()
-    {
-        return $GLOBALS['TSFE'];
+        $content = $this->tag->render();
+        $content .= $this->renderChildren();
+        $content .= '</div>';
+        return $content;
     }
 }
