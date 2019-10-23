77b8380c9d22c5dd2515d3bc462f62dc18095927
Jesper Paardekooper <j.paardekooper@develement.nl>
2019-10-23 12:19:33 +0200

    Remove version requirement

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,9 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'j.paardekooper@develement.nl',
     'author_company' => 'DevElement',
     'constraints' => [
-        'depends' => [
-            'typo3' => '6.2.0-8.99.99',
-        ],
+        'depends' => [],
         'conflicts' => [],
         'suggests' => [],
     ],
